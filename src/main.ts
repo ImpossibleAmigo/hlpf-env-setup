e64a47107efaac0d37c4ae0e4c1d8f6d3e4cdc8a
ImpossibleAmigo <cheberiako2016@gmail.com>
2026-04-27 18:08:27 +0300

    add LoggingInterceptor, TransformInterceptor, HttpExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 ﻿import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module.js';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor.js';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor.js';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new LoggingInterceptor(), new TransformInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('MiniShop API')
+    .setDescription('REST API для навчального інтернет-магазину. Автентифікація через JWT Bearer token.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
   console.log('Application is running on: http://localhost:3000');
 }
